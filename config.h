5a46687dfce27ab8ee3878e48b185166b42e0441
Danil Dulin <danil.dulin@gs-labs.ru>
2017-07-30 01:22:39 +0300

    Update config

--- config.h
+++ config.h
@@ -4,10 +4,10 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"Terminus:size=10",
-	"FontAwesome:size=10"
+	"Terminus:size=12",
+	"FontAwesome:size=12"
 };
-static const char dmenufont[]       = "Terminus:size=10";
+static const char dmenufont[]       = "Terminus:size=12";
 static const char normbordercolor[] = "#404040";
 static const char normbgcolor[]     = "#131314";
 static const char normfgcolor[]     = "#9B9696";
@@ -16,7 +16,7 @@ static const char selbgcolor[]      = "#131314";
 static const char selfgcolor[]      = "#408075";
 static const char *colors[SchemeLast][3]      = {
 	/*               fg           bg           border   */
-	[SchemeNorm] = { normfgcolor, normfgcolor, normbordercolor },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
 	[SchemeSel] =  { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
@@ -25,12 +25,12 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 0;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
 /* tagging                   0|0    1|1     2|2    3|3   4|4      5|F1      6|F2     7|F3    8|F4  */
-static const char *tags[] = {"dev", "term", "www", "im", "media", "screen", "build", "xmpp", "slack" };
+static const char *tags[] = {"dev", "git", "www", "im", "media", "app",     "build", "misc", "other" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -38,7 +38,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                        instance    title       tags mask     isfloating   monitor */
-	{ "chromium-browser-chromium",  NULL,       NULL,       1 << 2,       0,           -1},
+	{ "Google-chrome",              NULL,       NULL,       1 << 2,       0,           -1},
+	{ "google-chrome",              NULL,       NULL,       1 << 2,       0,           -1},
+	{ "Telegram",                   NULL,       NULL,       1 << 3,       0,           -1},
 };
 
 /* layout(s) */
@@ -49,12 +51,12 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[T]",      tile },    /* first entry is default */
-	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+#define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -65,16 +67,18 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* alsa */
-static const char *upvol[]   = { "amixer", "-c", "1", "set", "Master", "1.5dB+", NULL };
-static const char *downvol[] = { "amixer", "-c", "1", "set", "Master", "1.5dB-", NULL };
-static const char *mute[]    = { "amixer", "-q", "set", "Master", "toggle", NULL };
+static const char *upvol[]   = { "amixer", "-q", "-D", "pulse", "sset", "Master", "2%+", NULL };
+static const char *downvol[] = { "amixer", "-q", "-D", "pulse", "sset", "Master", "2%-", NULL };
+static const char *mute[]    = { "amixer", "-q", "-D", "pulse", "sset", "Master", "toggle", NULL };
 /* backlight */
-static const char *incbacklight[] = { "xbacklight", "-inc", "10", NULL };
-static const char *decbacklight[] = { "xbacklight", "-dec", "10", NULL };
+static const char *incbacklight[] = { "xbacklight", "-inc", "10", "-steps", "1", NULL };
+static const char *decbacklight[] = { "xbacklight", "-dec", "10", "-steps", "1", NULL };
+static const char *inc_kbd_backlight[] = { "xbacklight", "-ctrl", "asus::kbd_backlight", "-inc", "30", NULL };
+static const char *dec_kbd_backlight[] = { "xbacklight", "-ctrl", "asus::kbd_backlight", "-dec", "30", NULL };
 /* launchers */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]    = { "dmenu_run", "-m",dmenumon, "-fn",dmenufont, "-nb",normbgcolor, "-nf",normfgcolor, "-sb",selbgcolor, "-sf",selfgcolor, NULL };
-static const char *termcmd[]     = { "urxvtc", NULL };
+static const char *termcmd[]     = { "urxvt256c-mlc", NULL };
 static const char *telegramcmd[] = { "/opt/Telegram/Telegram", NULL };
 static const char *scrotcmd[]    = { "scrot", "-q", "100", "-e", "mv $f ~/Screenshots/", NULL };
 
@@ -87,6 +91,8 @@ static Key keys[] = {
 	/*                            Backlight Control                           */
 	{ 0,              XF86XK_MonBrightnessUp,        spawn,          {.v = incbacklight } },
 	{ 0,              XF86XK_MonBrightnessDown,      spawn,          {.v = decbacklight } },
+	{ 0,              XF86XK_KbdBrightnessUp,        spawn,          {.v = inc_kbd_backlight } },
+	{ 0,              XF86XK_KbdBrightnessDown,      spawn,          {.v = dec_kbd_backlight } },
 	/*                            Launchers                                  */
 	{ MODKEY,                       XK_p,            spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return,       spawn,          {.v = termcmd } },
@@ -100,22 +106,17 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,            pushup,         {0} },
 	{ MODKEY,                       XK_i,            incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,            incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,            setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,            setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_h,            setmfact,       {.f = -0.01} },
+	{ MODKEY,                       XK_l,            setmfact,       {.f = +0.01} },
 	{ MODKEY|ShiftMask,             XK_Return,       zoom,           {0} },
 	{ MODKEY,                       XK_Tab,          view,           {0} },
 	{ MODKEY,                       XK_q,            killclient,     {0} },
-	{ MODKEY,                       XK_t,            setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,            setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,            setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,        setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,        togglefloating, {0} },
+	{ MODKEY,                       XK_e,            setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_w,            setlayout,      {.v = &layouts[1]} },
+	{ ALTKEY,                       XK_space,        setlayout,      {0} },
+	{ ALTKEY|ShiftMask,             XK_space,        togglefloating, {0} },
 	{ MODKEY,                       XK_0,            view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,            tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,        focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period,       focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,        tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period,       tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_grave,                        0)
 	TAGKEYS(                        XK_1,                            1)
 	TAGKEYS(                        XK_2,                            2)
@@ -134,7 +135,7 @@ static Key keys[] = {
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[1]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
